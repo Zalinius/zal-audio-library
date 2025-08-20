21eb62dd8fb987864673651e14fd30d8e22f3136
Zalinius <zalinius@darzalgames.com>
2025-08-20 10:55:17 -0400

    java doc a bit

--- src/main/java/com/darzalgames/zalaudiolibrary/effects/tracking/EnvelopeReverser.java
+++ src/main/java/com/darzalgames/zalaudiolibrary/effects/tracking/EnvelopeReverser.java
@@ -3,6 +3,9 @@ package com.darzalgames.zalaudiolibrary.effects.tracking;
 import com.darzalgames.zalaudiolibrary.amplitude.Envelope;
 import com.darzalgames.zalaudiolibrary.pipeline.instants.MusicalInstant;
 
+/**
+ * A track effect that reverses an envelope of a musical instant, making sounds sound reversed
+ */
 public class EnvelopeReverser implements TrackEffect{
 
 	@Override
@@ -12,6 +15,11 @@ public class EnvelopeReverser implements TrackEffect{
 		return new MusicalInstant(instant.synth(), instant.pitch(), instant.duration(), reversedEnvelope, instant.amplitude(), instant.id());
 	}
 
+	/**
+	 * creates an envelope which is reversed from the original
+	 * @param original The envelope to reverse
+	 * @return A reversed envelope, which is like traversing the original envelope backwards
+	 */
 	public static Envelope reverseEnvelope(Envelope original) {
 		return (float envelopeDuration, float currentTime) -> original.getEnvelope(envelopeDuration, envelopeDuration-currentTime);
 	}
