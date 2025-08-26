9b896fa042a91d6af204a1b0839fa3d4664f38b0
Zalinius <zalinius@darzalgames.com>
2025-08-26 15:25:14 -0400

    Use synth and cleanup

--- src/main/java/com/darzalgames/zalaudiolibrary/bell/BellExperiment.java
+++ src/main/java/com/darzalgames/zalaudiolibrary/bell/BellExperiment.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.darzalgames.zalaudiolibrary.amplitude.percussive.ArEnvelope;
 import com.darzalgames.zalaudiolibrary.amplitude.percussive.PercussiveEnvelope;
+import com.darzalgames.zalaudiolibrary.synth.Synth;
 
 // An attempt to recreate this sound https://en.wikipedia.org/wiki/File:Additive_synthesis_bell.ogg
 public class BellExperiment {
@@ -16,7 +17,6 @@ public class BellExperiment {
 		float[] frequencyMultiples = {1.0f, 1.6766169f, 2.1741292f, 2.2537313f, 2.3830845f, 2.4925373f, 3.3731344f, 4.402985f, 5.437811f, 1.1641791f, 2.7363183f, 6.970149f, 8.40796f, 8.855721f, 9.960199f, 12.184079f, 13.1890545f, 14.373135f, 14.935324f, 16.169155f, 18.40796f};
 		float[] wobbleAmplitudes = {0f, 1f, .1f, .1f, .1f, 1, .1f, .2f, .2f, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		float[] wobbleFrequencies = {0, 2.8f, 9.2f, 9.5f, 10f, 4.8f, 9f, 8.8f, 7.1f, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		float[] amplitudeScales = {-32f, -34.5f, -39.8f, -50.2f, -45.6f, -35.6f, -35.7f, -36.3f, -38.4f, -45,-45,-45,-45,-45,-45,-45,-45,-50,-50,-50,-50};
 		float[] releaseTimes= {3.5f, 3.75f, 3.0f, 3.0f, 3.0f, 3.0f, 2.4f, 2.0f, 2.0f, 0.75f, 0.85f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.6f, 0.85f, 0.6f, 0.5f, 0.5f};
 
 		float[] amplitudes = {0.25f, 0.18f, 0.10f, 0.03f, 0.05f, 0.16f, 0.16f, 0.15f, 0.12f, 0.05f, 0.05f, 0.05f, 0.05f, 0.05f, 0.05f, 0.05f, 0.05f, 0.03f, 0.03f, 0.03f, 0.03f};
@@ -32,15 +32,17 @@ public class BellExperiment {
 
 
 	public static class Partial {
+		private final Synth synth;
 		private final float frequencyMultiplier;
-		private final float amplitude; //in decibels, the amplitude of this sound in particular
+		private final float amplitude;
+		private final PercussiveEnvelope envelope;
 
 		private final float amplitudeModulationAmplitude;
 		private final float amplitudeModulationFrequency;
 
-		private final PercussiveEnvelope envelope;
 
 		public Partial(float frequencyMultiplier, float wobbleAmplitude, float wobbleFrequency, float amplitude, float releaseTime) {
+			synth = Synth.sine();
 			this.frequencyMultiplier = frequencyMultiplier;
 			amplitudeModulationAmplitude = wobbleAmplitude;
 			amplitudeModulationFrequency = wobbleFrequency;
@@ -61,10 +63,9 @@ public class BellExperiment {
 			float amplitudeModulation = (float) Math.sin(amplitudeModulationPhase) * amplitudeModulationAmplitude + (1-amplitudeModulationAmplitude);
 
 
+			float toneComponent = synth.f((t * frequencyMultiplier * fundamentalFrequency)%1);
 
-			float toneComponent = (float) Math.sin(2*Math.PI * t * frequencyMultiplier * fundamentalFrequency);
-
-			return amplitude * amplitudeModulation * envelope.getEnvelope(t) * toneComponent; //TODO why multiply by 8? is the original just too quiet??
+			return amplitude * amplitudeModulation * envelope.getEnvelope(t) * toneComponent;
 		}
 
 
