a9d2fbf11c0e223ce06a0cfa71c295e7dcda1b70
Zalinius <zalinius@darzalgames.com>
2025-12-09 10:17:04 -0500

    Audio actors, and some effects

---
+++ src/main/java/com/darzalgames/zalaudiolibrary/effects/tracking/AmplifierEffect.java
@@ -0,0 +1,27 @@
+package com.darzalgames.zalaudiolibrary.effects.tracking;
+
+import com.darzalgames.zalaudiolibrary.pipeline.instants.MusicalInstant;
+
+public class AmplifierEffect extends SimpleMusicalEffect {
+
+	private float amplitude;
+
+	public AmplifierEffect(float amplitude) {
+		this.amplitude = amplitude;
+	}
+
+	public void setAmplitude(float newAmplitude) {
+		amplitude = newAmplitude;
+	}
+
+	@Override
+	public MusicalInstant applySimpleEffect(MusicalInstant original) {
+		float newAmplitude = multiplyAmplitudes(amplitude);
+		return new MusicalInstant(original.synth(), original.pitch(), original.frequencyModulator(), original.duration(), original.envelope(), newAmplitude, original.id());
+	}
+
+	public float multiplyAmplitudes(float originalAmplitude) {
+		return amplitude * originalAmplitude;
+	}
+
+}
---
+++ src/main/java/com/darzalgames/zalaudiolibrary/pipeline/AudioActor.java
@@ -0,0 +1,11 @@
+package com.darzalgames.zalaudiolibrary.pipeline;
+
+public interface AudioActor {
+
+	/**
+	 *
+	 * @param delta the change in seconds
+	 */
+	void update(float delta);
+
+}
