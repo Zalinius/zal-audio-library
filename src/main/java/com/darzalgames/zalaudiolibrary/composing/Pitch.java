173227a0d92b3068aee4ec1f2dc58d558d1d8c02
Zalinius <zalinius@darzalgames.com>
2026-07-14 20:13:41 -0400

    Sharps

--- src/main/java/com/darzalgames/zalaudiolibrary/composing/Pitch.java
+++ src/main/java/com/darzalgames/zalaudiolibrary/composing/Pitch.java
@@ -12,72 +12,113 @@ public class Pitch implements Comparable<Pitch> {
 	private final String name;
 	private final float frequency;
 
+	private static final NavigableSet<Pitch> allPitches = new TreeSet<>();
 	private static final NavigableSet<Pitch> naturalPitches = new TreeSet<>();
 
 	public static final Pitch NONE = new Pitch("None", 0f);
 
 	public static final Pitch C0 = new Pitch("C0", 16.35f);
+	public static final Pitch C0s = C0.sharpen();
 	public static final Pitch D0 = new Pitch("D0", 18.35f);
+	public static final Pitch D0s = D0.sharpen();
 	public static final Pitch E0 = new Pitch("E0", 20.60f);
 	public static final Pitch F0 = new Pitch("F0", 21.83f);
+	public static final Pitch F0s = F0.sharpen();
 	public static final Pitch G0 = new Pitch("G0", 24.50f);
+	public static final Pitch G0s = G0.sharpen();
 	public static final Pitch A0 = new Pitch("A0", 27.50f);
+	public static final Pitch A0s = A0.sharpen();
 	public static final Pitch B0 = new Pitch("B0", 30.87f);
 
 	public static final Pitch C1 = new Pitch("C1", 32.70f);
+	public static final Pitch C1s = C1.sharpen();
 	public static final Pitch D1 = new Pitch("D1", 36.71f);
+	public static final Pitch D1s = D1.sharpen();
 	public static final Pitch E1 = new Pitch("E1", 41.20f);
 	public static final Pitch F1 = new Pitch("F1", 43.65f);
+	public static final Pitch F1s = F1.sharpen();
 	public static final Pitch G1 = new Pitch("G1", 49.00f);
+	public static final Pitch G1s = G1.sharpen();
 	public static final Pitch A1 = new Pitch("A1", 55.00f);
+	public static final Pitch A1s = A1.sharpen();
 	public static final Pitch B1 = new Pitch("B1", 61.74f);
 
 	public static final Pitch C2 = new Pitch("C2", 65.41f);
+	public static final Pitch C2s = C2.sharpen();
 	public static final Pitch D2 = new Pitch("D2", 73.42f);
+	public static final Pitch D2s = D2.sharpen();
 	public static final Pitch E2 = new Pitch("E2", 82.41f);
 	public static final Pitch F2 = new Pitch("F2", 87.31f);
+	public static final Pitch F2s = F2.sharpen();
 	public static final Pitch G2 = new Pitch("G2", 98.00f);
+	public static final Pitch G2s = G2.sharpen();
 	public static final Pitch A2 = new Pitch("A2", 110.00f);
+	public static final Pitch A2s = A2.sharpen();
 	public static final Pitch B2 = new Pitch("B2", 123.47f);
 
 	public static final Pitch C3 = new Pitch("C3", 130.81f);
+	public static final Pitch C3s = C3.sharpen();
 	public static final Pitch D3 = new Pitch("D3", 146.83f);
+	public static final Pitch D3s = D3.sharpen();
 	public static final Pitch E3 = new Pitch("E3", 164.81f);
 	public static final Pitch F3 = new Pitch("F3", 174.61f);
+	public static final Pitch F3s = F3.sharpen();
 	public static final Pitch G3 = new Pitch("G3", 196.00f);
+	public static final Pitch G3s = G3.sharpen();
 	public static final Pitch A3 = new Pitch("A3", 220.00f);
+	public static final Pitch A3s = A3.sharpen();
 	public static final Pitch B3 = new Pitch("B3", 246.94f);
 
 	public static final Pitch C4 = new Pitch("C4", 261.63f);
+	public static final Pitch C4s = C4.sharpen();
 	public static final Pitch D4 = new Pitch("D4", 293.66f);
+	public static final Pitch D4s = D4.sharpen();
 	public static final Pitch E4 = new Pitch("E4", 329.63f);
 	public static final Pitch F4 = new Pitch("F4", 349.23f);
+	public static final Pitch F4s = F4.sharpen();
 	public static final Pitch G4 = new Pitch("G4", 392.00f);
+	public static final Pitch G4s = G4.sharpen();
 	public static final Pitch A4 = new Pitch("A4", 440.00f);
+	public static final Pitch A4s = A4.sharpen();
 	public static final Pitch B4 = new Pitch("B4", 493.88f);
 
 	public static final Pitch C5 = new Pitch("C5", 523.25f);
+	public static final Pitch C5s = C5.sharpen();
 	public static final Pitch D5 = new Pitch("D5", 587.33f);
+	public static final Pitch D5s = D5.sharpen();
 	public static final Pitch E5 = new Pitch("E5", 659.25f);
 	public static final Pitch F5 = new Pitch("F5", 698.46f);
+	public static final Pitch F5s = F5.sharpen();
 	public static final Pitch G5 = new Pitch("G5", 783.99f);
+	public static final Pitch G5s = G5.sharpen();
 	public static final Pitch A5 = new Pitch("A5", 880.00f);
+	public static final Pitch A5s = A5.sharpen();
 	public static final Pitch B5 = new Pitch("B5", 987.77f);
 
 	public static final Pitch C6 = new Pitch("C6", 1046.50f);
+	public static final Pitch C6s = C6.sharpen();
 	public static final Pitch D6 = new Pitch("D6", 1174.66f);
+	public static final Pitch D6s = D6.sharpen();
 	public static final Pitch E6 = new Pitch("E6", 1318.51f);
 	public static final Pitch F6 = new Pitch("F6", 1396.91f);
+	public static final Pitch F6s = F6.sharpen();
 	public static final Pitch G6 = new Pitch("G6", 1567.98f);
+	public static final Pitch G6s = G6.sharpen();
 	public static final Pitch A6 = new Pitch("A6", 1760.00f);
+	public static final Pitch A6s = A6.sharpen();
 	public static final Pitch B6 = new Pitch("B6", 1975.53f);
 
 	public static final Pitch C7 = new Pitch("C7", 2093.00f);
+	public static final Pitch C7s = C7.sharpen();
 	public static final Pitch D7 = new Pitch("D7", 2349.30f);
+	public static final Pitch D7s = D7.sharpen();
 	public static final Pitch E7 = new Pitch("E7", 2637.00f);
 	public static final Pitch F7 = new Pitch("F7", 2793.80f);
+	public static final Pitch F7s = F7.sharpen();
 	public static final Pitch G7 = new Pitch("G7", 3136.00f);
+	public static final Pitch G7s = G7.sharpen();
 	public static final Pitch A7 = new Pitch("A7", 3520.00f);
+	public static final Pitch A7s = A7.sharpen();
 	public static final Pitch B7 = new Pitch("B7", 3951.00f);
 
 	public static final Pitch C8 = new Pitch("C8", 4186.00f);
@@ -89,6 +130,7 @@ public class Pitch implements Comparable<Pitch> {
 	private Pitch(String name, float frequency, boolean natural) {
 		this.name = name;
 		this.frequency = frequency;
+		allPitches.add(this);
 		if (natural) {
 			naturalPitches.add(this);
 		}
@@ -210,6 +252,14 @@ public class Pitch implements Comparable<Pitch> {
 	@Override
 	public int compareTo(Pitch other) {
 		return Float.compare(frequency, other.frequency);
+	}
+
+	public static NavigableSet<Pitch> getAllpitches() {
+		return new TreeSet<>(allPitches);
+	}
+
+	public static NavigableSet<Pitch> getNaturalpitches() {
+		return new TreeSet<>(naturalPitches);
 	}
 
 }
